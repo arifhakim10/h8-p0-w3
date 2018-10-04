82328f5f505f63b30bfe13b116c2cb3a33e03245
arifhakim10 <arifrhakim10@yahoo.com>
2018-10-04 16:25:57 +0700

    exercise3

---
+++ exercise3.js
@@ -0,0 +1,25 @@
+
+
+var input = [
+  ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+  ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"], 
+  ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"], 
+  ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+
+]
+
+function dataHandling(str){
+  for(var i=0; i<str.length; i++){
+    console.log('Nomor ID: ' + str[i][0])
+    console.log('Nama Lengkap: ' + str[i][1])
+    console.log('TTL: ' + str[i][2] +' '+ str[i][3])
+    console.log('Hobi: ' + str[i][4])
+    console.log()
+
+  }
+}
+dataHandling(input)
+
+
+
+
