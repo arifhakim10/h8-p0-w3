8f63c06eb7a1d7722205f795e95a7cab59bbd2a9
arifhakim10 <arifrhakim10@yahoo.com>
2018-10-05 15:31:55 +0700

    exercise 5 week 3

---
+++ exercise5.js
@@ -0,0 +1,14 @@
+function palindrome(kata) {
+
+  for(var i = 0; i < kata.length; i++) {
+    for(var j = kata.length - 1; j >= 0; j--) {
+      return kata[i] === kata[j];
+    }
+  }
+}
+
+console.log(palindrome('katak'));
+console.log(palindrome('blanket'));
+console.log(palindrome('civic'));
+console.log(palindrome('kasur rusak'));
+console.log(palindrome('mister'));
