5386479bb13f21ea41b7484b40cc164fd0ec55e0
arifhakim10 <arifrhakim10@yahoo.com>
2018-10-06 20:46:40 +0700

    exercise 13 week 3

---
+++ exercise13.js
@@ -0,0 +1,36 @@
+
+function targetTerdekat(arr) {
+  var index1 = 0;
+  var index2 = 0;
+  var tampung = 0;
+  var arrX = [];
+  
+  if(arr.indexOf('x') === -1){
+    return 0
+  }
+  else{
+    for (var i = 0; i < arr.length; i++){
+      if (arr[i] === 'x'){
+      index1 = i
+      arrX.push(index1)
+      }
+      else if (arr[i] === 'o'){
+       index2 = i
+      }
+   
+      var hasil= [];
+      for (var j = 0; j < arrX.length; j++){
+        tampung = Math.abs(index2 - arrX[j]);
+        hasil.push(tampung)
+        hasil.sort()
+      }
+    }
+      return hasil[0]
+  }
+  }
+
+console.log(targetTerdekat(['', '', 'o', '', '', 'x', '', 'x']));
+console.log(targetTerdekat(['o', '', '', '', 'x', 'x', 'x']));
+console.log(targetTerdekat(['x', '', '', '', 'x', 'x', 'o', '']));
+console.log(targetTerdekat(['', '', 'o', '']));
+console.log(targetTerdekat(['', 'o', '', 'x', 'x', '', '', 'x']));
