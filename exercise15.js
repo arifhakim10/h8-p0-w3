67a0789d56c1afbc8c4ee235229b5e89a419d1d6
arifhakim10 <arifrhakim10@yahoo.com>
2018-10-07 13:04:56 +0700

    exercise 15 week 3

---
+++ exercise15.js
@@ -0,0 +1,27 @@
+function groupAnimals(animals) {
+    animals.sort(function(a, b){return a>b}) 
+    var tampung = []
+    var result = [] 
+    for(var i = 0; i < animals.length-1; i++) {
+        if(animals[i][0] === animals[i+1][0]) {
+            tampung.push(animals[i])
+            if(i === animals.length - 2) {
+                tampung.push(animals[i+1])
+                result.push(tampung)
+            }
+        } else if(animals[i][0] !== animals[i+1][0]) {
+            tampung.push(animals[i])
+            result.push(tampung)
+            tampung = []
+            if(i === animals.length-2) {
+                tampung.push(animals[i+1])
+                result.push(tampung)
+            }
+        }
+    }return result
+  }
+  
+  
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
